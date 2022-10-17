60b000ba44e71ee325592d2d9b8d16afa58b0c82
Alexey_Afonin <45484621+AlexeyAfonin@users.noreply.github.com>
2022-10-17 11:43:51 +0300

    Add HelpWindow

---
+++ Assets/Scripts/Windows/HelpWindow.cs
@@ -0,0 +1,18 @@
+using Core.Base;
+using Core.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public sealed class HelpWindow : BaseWindow
+{
+    [Space(10f)]
+    [SerializeField] private TextMeshProUGUI promptText;
+
+    public void ShowPrompt(string prompt)
+    {
+        promptText.text = prompt;
+        Show();
+    }
+}
